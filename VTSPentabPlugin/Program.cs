a708e428ac267a5b6b57eb7c3cf5d2e78eeda68f
Ganeesya <ganephone@gmail.com>
2021-10-11 00:22:19 +0900

    set version

--- VTSPentabPlugin/Program.cs
+++ VTSPentabPlugin/Program.cs
@@ -1,8 +1,7 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("0.1.0")]
 
 namespace VTSPentabPlugin
 {
